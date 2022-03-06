66d0e2ed0e9c8efaf1e4070f850262bcd74d3091
Curnowj0407 <99618992+Curnowj0407@users.noreply.github.com>
2022-03-07 10:23:01 +1300

    Add files via upload

---
+++ 01_yes_no_v3.py
@@ -0,0 +1,27 @@
+def yes_no(question):
+    valid = False
+    while not valid:
+        response  = input(question).lower()
+
+    
+    
+        if response == "yes" or response =="y": 
+                response = "yes"
+                return response
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response 
+        else:
+            print("please answer yes / no")
+
+show_instructions = yes_no("do you want to see the instructions?")            
+            
+print("you chose {}". format(show_instructions))    
+print()
+
+having_fun = yes_no("are you having fun?")
+print ("you said {} to having fun".format(having_fun))
+            
+         
+       
