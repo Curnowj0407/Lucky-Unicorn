60300e9592e423bc9fe243ef7d0f0305c01e8049
Curnowj0407 <99618992+Curnowj0407@users.noreply.github.com>
2022-03-24 12:59:40 +1300

    Add files via upload

--- 06_Rounds_v02.py
+++ 06_Rounds_v02.py
@@ -1,61 +1,7 @@
-def yes_no(question):
-    valid = False
-    while not valid:
-        response  = input(question).lower()
-
-    
-    
-        if response == "yes" or response =="y": 
-                return "yes"
-
-        elif response == "no" or response == "n":
-            return "no" 
-        else:
-            print("please answer yes / no")
-
-
-def instructions():
-    print("**** how to play ****")
-    print()
-    print("the rules of the game go here")
-    print()
-    return ""
-
-
-def num_check(question, low, high):
-
-    error = "Please enter an integer between 1 and 10\n"
-
-    valid = False
-    while not valid:
-        try:
-            # ask the question
-            response = int(input(question))
-
-            # if the amount is too low / too high give
-            if low < response <= high:
-                return response
-
-            else:
-                print(error)
-
-        except ValueError:
-            print(error)
-
-played_before = yes_no("have you played the game before?")
-
-
-if played_before == "no":
-    instructions()
-else:
-    ("program continues")
-
-
-# Ask user how much they to play with...
-how_much = num_check("how much would you like to play with? ", 0, 10)
 import random
 
-balance = how_much
+
+balance = 5
 
 rounds_played = 0
 
@@ -111,3 +57,7 @@ while play_again == "":
     else:
         play_again = input("press Enter to play again or 'xxx' to quit")
 
+
+print()
+print("final balance",
+      balance)
